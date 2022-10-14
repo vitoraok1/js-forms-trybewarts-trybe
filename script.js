c0c8076b0214911f45ddfc3123762922f235f469
jameesdavid <jameesdavid@outlook.com.br>
2022-10-14 19:39:20 -0300

    add validation on submit button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const buttonSend = document.getElementById('send-button');
 
+const check = document.getElementById('agreement');
+
 const login = {
   email: 'tryber@teste.com',
   password: '123456',
 };
 
-function alertNotify(button) {
-  console.log('teste');
+const alertNotify = (button) => {
   button.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
@@ -15,6 +16,16 @@ function alertNotify(button) {
   } else {
     alert('Email ou senha inválidos.');
   }
-}
+};
+
+const submitForm = () => {
+  const sendButton = document.getElementById('submit-btn');
+  const terms = document.getElementById('agreement');
+  sendButton.disabled = !terms.checked;
+};
+
+window.onload = () => {
+  check.addEventListener('change', submitForm);
+};
 
 buttonSend.addEventListener('click', alertNotify);
